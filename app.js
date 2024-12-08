e4d8431e720fc20e014cfdfecdd99f9bef716ec0
Cheifath <cheifathmohammed@gmail.com>
2024-12-08 20:42:16 +0530

    Add js

---
+++ app.js
@@ -0,0 +1,26 @@
+let boxes=document.querySelectorAll(".box");
+let resetbutton=document.querySelector(".res-button");
+let valueIsO="true";
+
+boxes.forEach((box)=>{
+    box.addEventListener("click",()=>{
+
+        //if the previous box was O or the valIsO is ture
+        if(valueIsO==="true"){
+        
+            //the content is set to X
+            box.textContent="X";
+            //updating the valis0 to false
+            valueIsO="false";
+        }
+
+        //if the previous box was X or the valIsO is false
+        else if(valueIsO==="false"){
+
+            //the content is set to O
+            box.textContent="O";
+            //updating the valIs0 to true
+            valueIsO="true";
+        }
+    });
+});
