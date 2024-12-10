aa633636e59629d50514c049dc5a7acc220020f7
Cheifath <cheifathmohammed@gmail.com>
2024-12-10 20:15:05 +0530

    Add winPattern

--- app.js
+++ app.js
@@ -1,6 +1,17 @@
 let boxes=document.querySelectorAll(".box");
 let resetbutton=document.querySelector(".res-button");
 let valueIsO="true";
+//positon of winners
+const winPatterns=[
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [0,3,6],
+    [1,4,7],
+    [2,5,8],
+    [2,4,6],
+    [0,4,8]
+];
 
 //box manipulation
 boxes.forEach((box)=>{
